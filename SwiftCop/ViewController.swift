a652ccbf6cb8f43c949f8f46ce629384e85e4da9
Andres Canal <andres@inakanetworks.com>
2015-10-16 20:58:52 -0300

    Initial Commit

---
+++ SwiftCop/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftCop
+//
+//  Created by Andres on 10/16/15.
+//  Copyright © 2015 Andres Canal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
